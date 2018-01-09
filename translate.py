306562a16ebd2eebf7591f482f30cc957eace05f
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-01-09 20:26:07 +0300

    Call translate api

--- translate.py
+++ translate.py
@@ -1,4 +1,34 @@
 from item_store import ItemStore
+import urllib
+import urllib2
+import json
+
+
+
+TRANSLATE_API_KEY = 'trnsl.1.1.20180108T204339Z.bd63133ee1faca13.5e5d65d48b26e37678d2c84ecfe2c195e3cabf9d'
+TRANSLATE_URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+
+
+
+def create_url(query):
+    data = urllib.urlencode({
+        'key' : TRANSLATE_API_KEY,
+        'lang' : 'en-ru'
+    })
+    return '{}?{}'.format(TRANSLATE_URL, data)
+
+
+
+def call_api(query):
+    url = create_url(query)
+    data = urllib.urlencode({
+        'text' : query
+    })
+    request = urllib2.urlopen(url, data)
+    result = json.dumps(request.read(), ensure_ascii=False)
+    return result
+
+
 
 def translate(query):
     store = ItemStore()
@@ -12,11 +42,7 @@ def translate(query):
     print store
 
 
+
 # for debug only
 if __name__ == '__main__':
-    store = ItemStore()
-    for i in range(0, 10):
-        store.add_item(
-                'title_{}'.format(i),
-                'subtitle_{}'.format(i))
-    print store
+    print call_api("Hello world!")
