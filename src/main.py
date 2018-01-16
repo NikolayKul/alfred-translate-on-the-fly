00ec8d6b9ecf84148ecd672c0bcceb6e7ff6c9f7
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-01-16 15:15:56 +0300

    Take `translate` logic into a separate file

--- src/main.py
+++ src/main.py
@@ -1,34 +1,14 @@
 from item_store import ItemStore
 from predict import predict_text
-import urllib
-import urllib2
-import json
+from translate import translate_text
 
 
-TRANSLATE_API_URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-TRANSLATE_API_KEY = 'trnsl.1.1.20180108T204339Z.bd63133ee1faca13.5e5d65d48b26e37678d2c84ecfe2c195e3cabf9d'
-
-
-def is_russian(query):
+def is_russian(text):
     try:
-        query.decode('ascii')
+        text.decode('ascii')
         return False
     except (UnicodeDecodeError, UnicodeEncodeError):
         return True
-
-
-def get_translate_lang(query):
-    return 'ru-en' if is_russian(query) else 'en-ru'
-
-
-def api_translate(query):
-    data = urllib.urlencode({
-        'key' : TRANSLATE_API_KEY,
-        'lang' : get_translate_lang(query),
-        'text' : query
-    })
-    request = urllib2.urlopen(TRANSLATE_API_URL, data)
-    return json.loads(request.read())
 
 
 def get_text_icon(text):
@@ -37,18 +17,24 @@ def get_text_icon(text):
 
 
 def translate(query):
+
     prediction = predict_text(query).encode('utf-8')
     print 'prediction = ' + prediction
-    result = api_translate(prediction)
-    print 'result_with_pred = ' + str(result)
+
+    result = translate_text(prediction)
+    print 'translate_prediction = ' + str(result)
+
     store = ItemStore()
     for text in result['text']:
         sub = '' if query == prediction else prediction
-        print 'sub = ' + sub.decode('utf-8')
-        print 'text = ' + text.decode('utf-8')
+        sub = sub.decode('utf-8')
+        text = text.decode('utf-8')
+        print 'sub = ' + sub
+        print 'text = ' + text
+
         store.add_item(
-                    title = text.decode('utf-8'),
-                    subtitle = sub.decode('utf-8'),
+                    title = text,
+                    subtitle = sub,
                     icon = get_text_icon(text))
     return store
 
